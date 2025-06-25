682e0cc962125b92cf153423a8816548fa8f0ac4
Yarnsh <yarnsh@gmail.com>
2025-06-24 20:47:55 -0400

    silly clicking on stuff

--- UI/horatius.gd
+++ UI/horatius.gd
@@ -1,5 +1,6 @@
 extends Sprite2D
 
+@onready var clicker = $Clicker
 @onready var anim = $Anim
 var emotes = [
 	load("res://Images/horatius-1.png"),
@@ -13,3 +14,16 @@ func set_emotion(e):
 	anim.play("Bounce")
 	
 	texture = emotes[e]
+
+func _input(event: InputEvent) -> void:
+	if visible and event.is_action_pressed("Click"):
+		var query := PhysicsPointQueryParameters2D.new()
+		query.collide_with_areas = true
+		query.collide_with_bodies = false
+		query.position = (event as InputEventMouse).global_position
+		var result := get_world_2d().direct_space_state.intersect_point(query)
+		var in_the_area := false
+		for entry in result:
+			if entry.collider == clicker:
+				set_emotion(2)
+				break
