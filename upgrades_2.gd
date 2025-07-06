298a27a5b51c3296dfb0ebf20682147dd0804c69
Yarnsh <yarnsh@gmail.com>
2025-07-06 15:47:11 -0400

    More backrooms work

--- upgrades_2.gd
+++ upgrades_2.gd
@@ -36,6 +36,22 @@ func _ready() -> void:
 			"description": "Cauldron to prepare potions for sale. This staple of any magic shop sells for a good price and loved by all."
 		}
 	)
+	$Gong.set_item(
+		{
+			"title": "Gong",
+			"price": 25000,
+			"callback": Callable.create(self, "buy_gong"),
+			"description": "A gong who's ring will dispell evil spirits. Gives a boost to vibe when used successfully."
+		}
+	)
+	$PrepStation.set_item(
+		{
+			"title": "Prep Station",
+			"price": 25000,
+			"callback": Callable.create(self, "buy_prep_station"),
+			"description": "A station to prepare orders ahead of time\nRequired for some recipes"
+		}
+	)
 
 func buy_accountant():
 	# TODO: enable tax-free and the chore
@@ -46,21 +62,35 @@ func buy_accountant():
 
 func buy_crystals():
 	GameGlobals.unlocked_recipes.append("crystal")
-	$CrystalBall.set_item({"sold_out": true})
+	$Crystals.set_item({"sold_out": true})
 	text.hide()
 
 func buy_incense():
 	GameGlobals.unlocked_recipes.append("incense")
-	$CrystalBall.set_item({"sold_out": true})
+	$Incense.set_item({"sold_out": true})
 	text.hide()
 
 func buy_cauldron():
 	GameGlobals.unlocked_recipes.append("potion")
-	$CrystalBall.set_item({"sold_out": true})
+	$Cauldron.set_item({"sold_out": true})
 	text.hide()
+
+func buy_gong():
+	GameGlobals.unlocked_recipes.append("gong")
+	$Gong.set_item({"sold_out": true})
+	text.hide()
+
+func buy_prep_station():
+	GameGlobals.prep_station_count += 1
+	if GameGlobals.prep_station_count >= 6:
+		$PrepStation.set_item({"sold_out": true})
+		text.hide()
 
 func _on_back_button_pressed() -> void:
 	get_parent().close_shop2()
 
 func on_open():
+	pass
+
+func poor_response():
 	pass
