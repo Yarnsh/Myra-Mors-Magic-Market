ae079e2bb4a793eeb77b2e3f043889e36786246f
Yarnsh <yarnsh@gmail.com>
2025-07-16 20:06:09 -0400

    Gong noises

--- gongtask.gd
+++ gongtask.gd
@@ -3,6 +3,9 @@ extends Node2D
 @export var control_texture : Texture2D
 @export var done_texture : Texture2D
 
+@onready var hit = $Hit
+@onready var hit_loop = $Success
+
 @onready var anim = $Anim
 @onready var hammer = $Hammer
 @onready var h_r = hammer.rotation
@@ -26,6 +29,7 @@ func take_input(sc : String):
 	if sc == "G":
 		holding = true
 	elif sc == "Enter":
+		hit_loop.stop()
 		GameGlobals.task_manager.report_result({
 			"quality": q,
 			"vibe_gain_mult": 5.0 * q
@@ -37,11 +41,14 @@ func release_input(sc : String):
 		if holding:
 			q = max(q, strength)
 			holding = false
+			hit.stop()
+			hit.play()
 			if strength < 1.0 and q < 1.0:
 				anim.stop()
 				anim.play("Ding")
 			else:
 				anim.play("Bong")
+				hit_loop.play()
 
 func _process(delta: float) -> void:
 	if holding:
